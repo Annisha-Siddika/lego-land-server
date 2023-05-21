7760eb50053bc93390f1aaae48e1f6cdf9fd4fba
AnnishaSiddika <annisha2112@gmail.comm>
2023-05-21 16:24:59 +0600

    post method for add a toy

--- index.js
+++ index.js
@@ -27,18 +27,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const categoriesCollection = client.db('legoLand').collection('categories');
+    const allToysCollection = client.db('legoLand').collection('allToys');
 
     // get the sub category data 
     app.get('/categories', async(req, res)=>{
         const category = req.query.category;
         try {
             const data = await categoriesCollection.find({ category: category }).toArray();
-            res.json(data);
+            res.send(data);
           } catch (error) {
             console.error('Error fetching data from database:', error);
-            res.status(500).send('Internal Server Error');
+            res.send('Internal Server Error');
           }
     })
+
 
     // get all single category data 
     app.get('/categories/:id', async(req, res)=>{
@@ -47,7 +49,22 @@ async function run() {
 
         const result = await categoriesCollection.findOne(query);
         res.send(result)
+    }) 
+
+    // to test vercel deploy
+    app.get('/alltoys', async(req, res)=>{
+        const cursor = categoriesCollection.find();
+
+        const result = await cursor.toArray()
+        res.send(result)
     })
+
+    app.post('/addtoys', async(req, res)=>{
+      const addToys = req.body;
+      const result = await allToysCollection.insertOne(addToys);
+      res.send(result);
+  })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
